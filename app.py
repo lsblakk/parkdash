fa351243300021d009c1bdced53c9f88a7a63049
Lukas Blakk <lukasblakk@gmail.com>
2012-04-07 15:44:37 -0700

    testing out landing page creation, jquery mobile

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 import os
+from flask import Flask, request, redirect, render_template
 
-from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello World!'
+    return render_template('test_page.html')
+    # return 'Hello World!'
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
